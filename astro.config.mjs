9893c6b70824f98d02e183b462ce9d151632ae2f
Ulyzses <ulyzses.code@gmail.com>
2023-01-27 23:49:32 +0800

    Converted to SSR and added form functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+    output: 'server',
+    adapter: netlify(),
+    integrations: [react()]
 });
